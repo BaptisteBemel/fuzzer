098a3a7962031fc53236369fbe65ca81ee10b767
clement-alloin-ulb <clement@Ubuntu22.myguest.virtualbox.org>
2024-03-06 16:24:50 +0100

    Add functions

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #define TVERSION "00"
 
 char* path ;
+int number_of_success = 1 ;
 
 struct tar_t
 {                              /* byte offset */
@@ -66,6 +67,14 @@ static struct tar_t header;
     } else {
         printf("Crash message\n");
         rv = 1;
+
+        //Rename file
+        char new_name[200];
+        snprintf(new_name, sizeof(new_name), "success_%d.tar", number_of_success);
+
+        rename("archive.tar", new_name);
+        number_of_success++ ;
+
         goto finally;
     }
     finally:
@@ -103,6 +112,7 @@ unsigned int calculate_checksum(struct tar_t* entry){
 void create_header(struct tar_t* header) {
     
     char archive_name[100];
+    snprintf(archive_name, sizeof(archive_name), "archive_.tar");
     char linkname[100] = "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
     char full_zero[8] = "0000000";
 
@@ -164,16 +174,172 @@ void gen_tar(struct tar_t* header) {
 }
 
 /**
+Pour le debug 
+ */
+void print_header(struct tar_t* header) {
+    printf("-----Header start-----\n");
+    printf("Name:      %s\n", header->name);
+    printf("Mode:      %s\n", header->mode);
+    printf("UID:       %s\n", header->uid);
+    printf("GID:       %s\n", header->gid);
+    printf("Size:      %s\n", header->size);
+    printf("Mtime:     %s\n", header->mtime);
+    printf("Chksum:    %s\n", header->chksum);
+    printf("Typeflag:  %c\n", header->typeflag);
+    printf("Linkname:  %s\n", header->linkname);
+    printf("Magic:     %s\n", header->magic);
+    printf("Version:   %s\n", header->version);
+    printf("Uname:     %s\n", header->uname);
+    printf("Gname:     %s\n", header->gname);
+    printf("Devmajor:  %s\n", header->devmajor);
+    printf("Devminor:  %s\n", header->devminor);
+    printf("Prefix:    %s\n", header->prefix);
+    printf("Padding:   %s\n", header->padding);
+    printf("-----Header end-----\n");
+}
+
+//Fuzzing on the field name
+void name(){
+    printf(" Start of Fuzzing on NAME\n");
+
+
+
+    printf(" End of Fuzzing on MODE\n");
+}
+
+//Fuzzing on the field mode
+void mode(){
+    printf(" Start of Fuzzing on MODE\n");
+
+
+
+    printf(" End of Fuzzing on MODE\n");
+}
+
+//Fuzzing on the field uid
+void uid(){
+    printf(" Start of Fuzzing on UID\n");
+
+
+
+    printf(" End of Fuzzing on UID\n");
+}
+
+//Fuzzing on the field gid
+void gid(){
+    printf(" Start of Fuzzing on GID\n");
+
+
+
+    printf(" End of Fuzzing on GID\n");
+}
+
+//Fuzzing on the field size
+void size(){
+    printf(" Start of Fuzzing on SIZE\n");
+
+
+
+    printf(" End of Fuzzing on SIZE\n");
+}
+
+//Fuzzing on the field mtime
+void mtime(){
+    printf(" Start of Fuzzing on MTIME\n");
+
+
+
+    printf(" End of Fuzzing on MTIME\n");
+    
+}
+
+//Fuzzing on the field chksum
+void chksum(){
+    printf(" Start of Fuzzing on CHKSUM\n");
+
+
+
+    printf(" End of Fuzzing on CHKSUM\n");
+    
+}
+
+//Fuzzing on the field typeflag
+void typeflag(){
+
+    printf(" Start of Fuzzing on TYPEFLAG\n");
+
+    // Brute-force every possible value from 0 to 255, possible since typeflag is a single-byte character
+    for (int i = 0; i < 256; i++){
+        create_header(&header);
+        header.typeflag = (char) i;
+        gen_tar(&header);
+        extractor() ;
+    }
+
+    printf(" End of Fuzzing on TYPEFLAG\n");
+}
+
+//Fuzzing on the field linkname
+void linkname(){
+    printf(" Start of Fuzzing on LINKNAME\n");
+
+
+
+    printf(" End of Fuzzing on LINKNAME\n");
+    
+}
+
+//Fuzzing on the field magic
+void magic(){
+    printf(" Start of Fuzzing on MAGIC\n");
+
+
+
+    printf(" End of Fuzzing on MAGIC\n");
+    
+}
+
+//Fuzzing on the field version
+void version(){
+    printf(" Start of Fuzzing on VERSION\n");
+
+
+
+    printf(" End of Fuzzing on VERSION\n");
+    
+}
+
+//Fuzzing on the field uname
+void uname(){
+    printf(" Start of Fuzzing on UNAME\n");
+
+
+
+    printf(" End of Fuzzing on UNAME\n");
+    
+}
+
+//Fuzzing on the field gname
+void gname(){
+    printf(" Start of Fuzzing on GNAME\n");
+
+
+
+    printf(" End of Fuzzing on GNAME\n");
+    
+}
+
+
+/**
  * Perform fuzzing on various field
  */
 void fuzzing() {
 
-    //gen_tar() ;
-
-
     create_header(&header);
 
     gen_tar(&header);
+
+    print_header(&header) ;
 
     extractor() ;
 
@@ -185,12 +351,24 @@ Main Function
 */
 int main(int argc, char* argv[])
 {
+    path = argv[1] ;
     
     printf("--- This is a fuzzing test ---\n") ;
 
-    path = argv[1] ;
-
     fuzzing() ;
+    name() ;
+    mode() ;
+    uid() ;
+    gid() ;
+    size() ;
+    mtime();
+    chksum();
+    typeflag() ;
+    linkname();
+    magic() ;
+    version();
+    uname();
+    gname();
 
-    printf("--- End of the fuzzing test ---\n") ;
+    printf("\n--- End of the fuzzing test ---\n") ;
 }
